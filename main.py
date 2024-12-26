9fd61230d0deb0d5d484f9dfd88a0e8372bc5ad2
Alexander Pozhidaev <pozhsasa090904@gmail.com>
2024-12-26 11:51:53 +0300

    included routers and run configuration

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+import uvicorn
 
 from db.database import db_manager
-
+from auth.auth_router import auth_router
+from api.profile.user_profile import user_profile
+from core.config import settings
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,13 +16,25 @@ async def lifespan(app: FastAPI):
     await db_manager.engine_dispose()
 
 
-
 main_app = FastAPI(
     lifespan=lifespan
 )
 
-@main_app.get("/")
+# including routers here
+main_app.include_router(auth_router)
+main_app.include_router(user_profile)
+
+@main_app.get("/", tags=["Greeting"], description="This endpoint sends just 'Hello!'")
 def hello():
     return {
         "message": "Hello!"
     }
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.api.host,
+        port=int(settings.api.port),
+        reload=True,
+    )
