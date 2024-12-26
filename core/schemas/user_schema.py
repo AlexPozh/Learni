95d38fd4832e377d988a20f03df05336b8c88300
Alexander Pozhidaev <pozhsasa090904@gmail.com>
2024-12-26 12:05:09 +0300

    moved schemas/ folder

--- core/schemas/user_schema.py
+++ core/schemas/user_schema.py
@@ -1,16 +1,24 @@
+from datetime import date
+
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class CreateUser(BaseModel):
     model_config = ConfigDict(strict=True)
-    
+   
     email: EmailStr
-    password: bytes
+    hash_password: str
+    username: str | None = None
+
+
+class CreateUserDB(CreateUser):
+    hash_password: bytes
+    is_email_confirmed: bool = False
 
 class UserSchema(BaseModel):
     model_config = ConfigDict(strict=True)
 
     username: str
     email: EmailStr
-    password: bytes
-    
+    is_email_confirmed: bool
+    registered_at: date
