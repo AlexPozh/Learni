d2a1b8c827f504a6da32d2529d32e3bc43221a90
Alexander Pozhidaev <pozhsasa090904@gmail.com>
2025-03-01 20:59:23 +0300

    created new get/post endpoints

--- auth/auth_router.py
+++ auth/auth_router.py
@@ -1,40 +1,70 @@
 import logging
+import uuid
 
-from fastapi import APIRouter, Depends, Form
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from email_validator import EmailNotValidError
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import RedirectResponse, ORJSONResponse
+from redis import Redis
 
-from core.schemas.user_schema import UserSchema, CreateUser, CreateUserDB
-from core.schemas.token_schema import TokenInfo
-from core.exceptions.auth_exception import UserUnauthorized
+from core.schemas.user_schema import UserSchema, CreateUser, CreateUserDB, LoginUser, NewPasswordScheme, NewPasswordDB
+from core.schemas.code_schema import EmailScheme, CodeScheme
+from core.exceptions.auth_exception import UserUnauthorized, WrongVerifCode
+from core.exceptions.user_exception import UserAlreadyExists
 from .auth_utils import validate_password, encode_jwt, hash_password
-from db.crud.user import get_user_by_email, create_user
+from services.send_code import send_code, compare_codes, get_redis_conn, cache_code, is_code_exists
+from db.crud.user import get_user_by_email, create_user, update_password
 from db.models.user import User
 from db.database import db_manager
 
 logger = logging.getLogger("development")
 
-auth_router = APIRouter(prefix="/auth", tags=["JWT"])
+auth_router = APIRouter(tags=["JWT"])
 
+templates = Jinja2Templates(directory="templates")
 
+# FUNCTIONS HELPERS-------------------------------------------------------------------------------
 async def validate_auth_user(
-    session: AsyncSession = Depends(db_manager.session_getter),
-    username: str = Form(),
-    password: str = Form()
+    user_log: LoginUser,
+    session: AsyncSession = Depends(db_manager.session_getter)
 ) -> User:
-    if not (user := await get_user_by_email(session, username)):
-        raise UserUnauthorized
+    try:
+        if not (user := await get_user_by_email(session, user_log.email)):
+            raise UserUnauthorized
     
-    if validate_password(
-        password=password,
-        hashed_pwd=user.hash_password
-    ):
-        return user
-    raise UserUnauthorized
+        if validate_password(
+            password=user_log.password,
+            hashed_pwd=user.hash_password
+        ):
+            return user
+        raise UserUnauthorized
+    except Exception as error:
+        logger.exception("%r", error)
+        raise UserUnauthorized
+
+async def is_user_exists(
+    user_email: EmailScheme,
+    session: AsyncSession
+) -> bool:
+    print(f"is_user_exists - f{user_email}")
+
+    try:
+        if not (user := await get_user_by_email(session, user_email.email)):
+            raise UserUnauthorized
+        else:
+            return True
+    except Exception as error:
+        logger.exception("%r", error)
+        raise UserUnauthorized
 
 
-@auth_router.post("/login/", response_model=TokenInfo)
+# POST requests-------------------------------------------------------------------------------
+@auth_router.post("/auth/login/")
 async def login(
-    user: UserSchema = Depends(validate_auth_user)
+    user: UserSchema = Depends(validate_auth_user),
 ):
     jwt_payload = {
         "sub": user.email,
@@ -43,16 +73,21 @@ async def login(
     token = encode_jwt(
         payload=jwt_payload
     )
-    return TokenInfo(
-        access_token=token,
-        token_type="Bearer"
+    response = RedirectResponse(url="/home", status_code=status.HTTP_303_SEE_OTHER)
+    response.set_cookie(
+        key="access_token",
+        value=token,
+        httponly=True,
+        samesite="strict",
+        max_age=300
     )
+    return response
 
 
-@auth_router.post("/register/", response_model=TokenInfo)
+@auth_router.post("/auth/register/")
 async def register(
     user: CreateUser,
-    session: AsyncSession = Depends(db_manager.session_getter)
+    session: AsyncSession = Depends(db_manager.session_getter),
 ):
     hashed_pwd = hash_password(user.hash_password)
     try:
@@ -66,15 +101,129 @@ async def register(
         )
         jwt_payload = {
             "sub": user.email,
-            "username": user.username if user.username else "no_username",
+            "username": user.username if user.username else "unknown",
         }
         token = encode_jwt(
             payload=jwt_payload
         )
-        return TokenInfo(
-            access_token=token,
-            token_type="Bearer"
+
+        response = RedirectResponse(url="/home?new_user=true", status_code=status.HTTP_303_SEE_OTHER)
+        response.set_cookie(
+            key="access_token",
+            value=token,
+            httponly=True,
+            samesite="strict",
+            max_age=300
         )
+        return response
+
+    except IntegrityError as error:
+        raise UserAlreadyExists
 
     except Exception as error:
         logger.exception("%r", error)
+
+
+@auth_router.post("/confirm_code/")
+async def post_confirm_code(
+    code: CodeScheme,
+    redis: Redis = Depends(get_redis_conn)
+):
+    try:
+        if compare_codes(code.email, code.code, redis):
+            token = str(uuid.uuid4())
+            cache_code(code.email, token, redis)
+            return RedirectResponse(url=f"/new_password/?email={code.email}", status_code=status.HTTP_303_SEE_OTHER)
+        else:
+            raise WrongVerifCode
+    
+    except WrongVerifCode:
+        raise WrongVerifCode
+    except Exception as error:
+        logger.exception("%r", error)
+
+
+@auth_router.post("/send_code/")
+async def post_send_code(
+    email: EmailScheme,
+    # is_user_exists: bool = Depends(is_user_exists),
+    redis: Redis = Depends(get_redis_conn),
+    session: AsyncSession = Depends(db_manager.session_getter)
+):
+    try:
+        logger.debug("Inside /send_code/")
+        if await is_user_exists(email, session):
+            send_code(email.email, redis)
+            return ORJSONResponse(content={"message": f"Code was sent successfully to {email.email}."}, status_code=200)
+    
+    except EmailNotValidError:
+        raise UserUnauthorized
+    except UserUnauthorized:
+        raise UserUnauthorized
+    except Exception as error:
+        logger.exception("%r", error)
+  
+
+@auth_router.post("/set_new_password/")
+async def post_new_password(
+    data: NewPasswordScheme,
+    session: AsyncSession = Depends(db_manager.session_getter)
+):
+    try:
+        new_hashed_password = hash_password(data.password)
+        await update_password(session, NewPasswordDB(
+                                                    email=data.email, 
+                                                    hash_password=new_hashed_password))
+        
+        return ORJSONResponse(content={"message": "Password changed"}, status_code=200)
+    except Exception as error:
+        logger.exception("%r", error)
+
+# GET requests-------------------------------------------------------------------------------
+@auth_router.get("/new_password/")
+async def get_new_password(
+    email: str,
+    request: Request,
+    redis: Redis = Depends(get_redis_conn)
+):
+    code = is_code_exists(email, redis)
+    if code is None:
+        return RedirectResponse("/login")
+    
+    return templates.TemplateResponse(
+        "new_password.html",
+        {
+            "request": request
+        }
+    )
+
+
+@auth_router.get("/forgot_password/")
+async def get_forgot_pwd(
+    request: Request,
+):
+    return templates.TemplateResponse(
+        "forgot_pwd.html",
+        {
+            "request": request
+        }
+    )
+
+@auth_router.get("/login/")
+async def get_login(request: Request):
+    return templates.TemplateResponse(
+        "login.html",
+        {
+            "request": request
+        }
+    )
+
+
+@auth_router.get("/register/")
+async def get_register(request: Request):
+    return templates.TemplateResponse(
+        "register.html",
+        {
+            "request": request
+        }
+    )
