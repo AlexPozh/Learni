db68ec51083347cafddebfcecb54836f4abdd997
Alexander Pozhidaev <pozhsasa090904@gmail.com>
2025-03-01 20:57:58 +0300

    added update_password function

--- db/crud/user.py
+++ db/crud/user.py
@@ -1,9 +1,9 @@
 
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..models.user import User
-from core.schemas.user_schema import CreateUserDB
+from core.schemas.user_schema import CreateUserDB, NewPasswordDB
 
 async def get_user_by_email(
     session: AsyncSession,
@@ -22,3 +22,11 @@ async def create_user(
     session.add(user)
     await session.commit()
     return user
+
+async def update_password(
+    session: AsyncSession,
+    update_pass: NewPasswordDB
+):
+    stmt = update(User).where(User.email == update_pass.email).values(hash_password=update_pass.hash_password)
+    await session.execute(stmt)
+    await session.commit()
